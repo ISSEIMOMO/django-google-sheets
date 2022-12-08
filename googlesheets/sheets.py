2d97b038f8635a9b79115f9e549f43cb7f8e6a69
ISSEIMOMO <88165272+ISSEIMOMO@users.noreply.github.com>
2022-12-08 11:55:55 -0300

    Update sheets.py

--- googlesheets/sheets.py
+++ googlesheets/sheets.py
@@ -1,6 +1,17 @@
 import gspread
 from django.conf import settings
 import os
+import json
+
+def add(self):
+    from django.core import serializers
+    adr = [self.pk,str(serializers.serialize('json', [self]))]
+    ad = adr.split("[")
+    ad = ad[1].split("]")
+    ad = ad[0]
+    ad = json.loads(ad)
+    ad = str(ad['model']).split('.')
+    return adr, str(ad[1])
 
 
 class Sheets:
@@ -18,7 +29,24 @@ class Sheets:
             worksheet = self.sh.worksheet(worksheet)
             return worksheet.get_all_values()
 
-    def adicionar(self,add,worksheet=None):
+
+    def lertabmod(self,se):
+        add, ws = add(se)
+        worksheet = self.sh.worksheet(ws)
+        lis = []
+        for li in worksheet.col_values(2):
+            s = li.split("[")
+            s = s[1].split("]")
+            lis.append(s[0])
+        lis = str(lis)
+        lista = lis.replace("['",'[')
+        lista = lista.replace("']",']')
+        lista = lista.replace("'",'')
+        return json.loads(lista)
+
+
+    def adicionar(self,se):
+        add, worksheet = add(se)
         if worksheet == None:
             self.sh.values_append(f'{self.SHEETS_WORK}!A1', params={'valueInputOption': 'RAW'}, body={'values': [add]})
         else:
@@ -26,7 +54,9 @@ class Sheets:
             return self.ler(worksheet=worksheet)
         return self.ler()
 
-    def updata(self,add,antigo,worksheet=None):
+    def updata(self,se,antigo=None,worksheet=None):
+        antigo = se.pk
+        add, worksheet = add(se)
         x = 1
         y = None
         if worksheet == None:
@@ -45,7 +75,10 @@ class Sheets:
             self.worksheet.update(f'A{y}', [add])
             return True
 
-    def delete(self,antigo,worksheet=None):
+    def delete(self,se,worksheet=None):
+        self.restaurar(se)
+        antigo = se.pk
+        add, worksheet = add(se)
         x = 1
         y = None
         if worksheet == None:
@@ -62,5 +95,26 @@ class Sheets:
         else:
             self.worksheet.delete_row(y)
             return True
+
+    def add(self, se):
+        salvar = None
+        if self.pk:
+            salvar = 1
+        else:
+            salvar = 0
+        self.salvar = salvar
+
+    def enviar(self, se):
+        if self.salvar == 1:
+            self.updata(se)
+        elif self.salvar == 0:
+            self.adicionar(se)
+        self.restaurar(se)
+
+    def restaurar(self, se):
+        add, worksheet = add(se)
+        google_sheets.lertabmod(worksheet)
+        serializers.deserialize("json", submateria.restaurar(), ignorenonexistent=True)
+
 
 google_sheets = Sheets()
